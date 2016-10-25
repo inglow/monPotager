034e9965fce58770131e1173e20f18f12d436e86
inglow <jouanny.jerome@gmail.com>
2016-10-25 12:42:46 +0200

    Public ROOm

--- publicRoom.php
+++ publicRoom.php
@@ -5,10 +5,6 @@ require_once("mesFonctions.php");
 			$result=getRoom();
 
 			$json_data = json_decode(getRoom(), true); 
-				echo $json_data['rooms']['name'];
-				foreach ($json_data as $value) {
-					# code...
-			echo $value['rooms']['name'];		
-
-				}
+			
+			echo $result;
  ?>
